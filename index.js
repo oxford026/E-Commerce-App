b8ae3c70ef38294b47e5095e2fbda64d04ed8381
oxford026 <153386275+oxford026@users.noreply.github.com>
2025-02-24 18:04:06 +0300

    update

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  res.send('hi there');
+    res.send(`
+    <div>
+      <form method="POST">
+        <input type="text" name="email" placeholder="Email" />
+        <input type="password" name="password" placeholder="Password" />
+        <input type="password" name="passwordConfirmation" placeholder="Password confirmation" />
+        <button>Sign Up</button>
+     
+      </form> 
+    </div>`);
+});
+
+
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('Account created!!!');
 });
 
 app.listen(3000, () => {
-  console.log('Listening');
+    console.log('Running');
 });
