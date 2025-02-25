ba3712d241576280bf2af91fc888352d83c7b6d1
oxford026 <153386275+oxford026@users.noreply.github.com>
2025-02-25 20:55:16 +0300

    update

---
+++ repositories/users.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+class usersRepository {
+  constructor(filename) {
+    if (!filename) {
+      throw new Error('Creating a repository requires a filename');
+    }
+
+    this.filename = filename;
+    try {
+    fs.accessSync(this.filename);
+    } catch (err) {
+      fs.writeFileSync(this.filename, '[]');
+    }
+  }
+  async getAll() {
+    const contents = await fs.promises.readFile(this.filename, {
+      encoding: 'utf8'
+    });
+    console.log(contents);
+  }
+
+}
+const test = async () => {
+  const repo = new usersRepository('users.json');
+  await repo.getAll();
+};
+test();
+
