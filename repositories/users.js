b6b1e2d2c5df09d6650e30bd80a348871bb9b31c
oxford026 <153386275+oxford026@users.noreply.github.com>
2025-02-28 16:25:47 +0300

    update

--- repositories/users.js
+++ repositories/users.js
@@ -1,29 +1,47 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 class usersRepository {
-  constructor(filename) {
-    if (!filename) {
-      throw new Error('Creating a repository requires a filename');
+    constructor(filename) {
+        if (!filename) {
+            throw new Error('Creating a repository requires a filename');
+        }
+
+        this.filename = filename;
+        try {
+            fs.accessSync(this.filename);
+        } catch (err) {
+            fs.writeFileSync(this.filename, '[]');
+        }
+    }
+    async getAll() {
+        return JSON.parse(
+            await fs.promises.readFile(this.filename, {
+                encoding: 'utf8'
+            }));
     }
 
-    this.filename = filename;
-    try {
-    fs.accessSync(this.filename);
-    } catch (err) {
-      fs.writeFileSync(this.filename, '[]');
-    }
-  }
-  async getAll() {
-    const contents = await fs.promises.readFile(this.filename, {
-      encoding: 'utf8'
-    });
-    console.log(contents);
-  }
+    async create(attrs) {
+        attrs.id = this.randomId();
+        const records = await this.getAll();
+        records.push(attrs);
+        await this.writeAll(records);
 
+    }
+    async writeAll(records) {
+        await fs.promises
+            .writeFile(this.filename, JSON.stringify(records, null, 2));
+    }
+    randomId() {
+        return  crypto.randomBytes(4).toString('hex');
+    }
 }
+
 const test = async () => {
-  const repo = new usersRepository('users.json');
-  await repo.getAll();
+    const repo = new usersRepository('users.json');
+    await repo.create({ email: 'test@test.com', password: 'password' });
+    const users = await repo.getAll();
+    console.log(users);
 };
 test();
 
