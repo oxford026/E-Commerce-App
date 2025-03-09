b07b6cb481bdb57d659a018d8166d1f97fb7d7e9
oxford026 <153386275+oxford026@users.noreply.github.com>
2025-03-09 23:29:39 +0300

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cookieSession({
     keys: ['q53w64se57dr6ft8g79yhouijpoyiuyt']
 }))
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
     res.send(`
     <div>
     ${req.session.userId ? 'You are logged in' : 'You are NOT logged in'}
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 
 
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
     const { email, password, passwordConfirmation } = req.body;
     const existingUser = await usersRepo.getOneBy({ email });
     if (existingUser) {
@@ -42,6 +42,36 @@ app.post('/', async (req, res) => {
     res.send('Account created!!!');
 });
 
+app.get('/signout', (req, res) => {
+    req.session = null;
+    res.send('You are logged out');
+})
+
+app.get('/signin', (req, res) => {
+    res.send(`
+    <div>
+      <form method="POST">
+        <input type="text" name="email" placeholder="Email" />
+        <input type="password" name="password" placeholder="Password" />
+        <button>Sign In</button>
+     
+      </form> 
+    </div>`);
+});
+
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await usersRepo.getOneBy({ email });
+    if (!user) {
+        return res.send('Email not found');
+    }
+    if (user.password !== password) {
+        return res.send('Invalid password');
+    }
+
+    req.session.userId = user.id;
+    res.send('You are signed in');
+});
 
 app.listen(3000, () => {
     console.log('Running');
