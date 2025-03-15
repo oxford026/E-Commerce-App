8897b5eac5540723d1c9ba7758e6588c37cb9493
oxford026 <153386275+oxford026@users.noreply.github.com>
2025-03-15 21:12:38 +0300

    products route

---
+++ routes/admin/products.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+router.get('/admin/product', (req, res) => {
+
+});
+
+router.get('/admin/products/new', (req, res) => {
+
+});
+
+module.exports = router;
