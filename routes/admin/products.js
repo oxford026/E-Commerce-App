4b6a69a6cf2fb17a670b19943a5c628a3c86ec90
oxford026 <153386275+oxford026@users.noreply.github.com>
2025-03-15 22:09:37 +0300

    Quick validations

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const { validationResult } = require('express-validator');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const router = express.Router();
-
+const { requireTitle, requirePrice } = require('./validators');
 
 router.get('/admin/product', (req, res) => {
 
@@ -11,7 +12,21 @@ router.get('/admin/product', (req, res) => {
 });
 
 router.get('/admin/products/new', (req, res) => {
+
+
     res.send(productsNewTemplate({}));
+
+});
+
+router.post('/admin/products/new', 
+    [requireTitle, 
+    requirePrice], 
+    
+    (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+
+    res.send('submitted');
 });
 
 module.exports = router;
