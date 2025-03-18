9158788a4491bc3c1bca16f4ef805ee4ab31bfc2
oxford026 <153386275+oxford026@users.noreply.github.com>
2025-03-19 02:06:34 +0300

    Deleting Products

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -13,8 +13,8 @@ const { requireTitle, requirePrice } = require('./validators');
 
 
 
-router.get('/admin/products', requireAuth , async (req, res) => {
-    
+router.get('/admin/products', requireAuth, async (req, res) => {
+
     const products = await productsRepo.getAll();
     res.send(productsIndexTemplate({ products }));
 
@@ -30,51 +30,56 @@ router.get('/admin/products/new', requireAuth, (req, res) => {
 });
 
 router.post('/admin/products/new',
-    requireAuth, 
+    requireAuth,
     upload.single('image'),
-    [requireTitle, 
-    requirePrice],
+    [requireTitle,
+        requirePrice],
     handleErrors(productsNewTemplate),
 
-    async(req, res) => {
+    async (req, res) => {
 
-    if (!req.session.userId) {
-        return res.redirect('/signin');
-    }
-    const image = req.file.buffer.toString('base64');
-    const { title, price } = req.body;
-    await productsRepo.create({ title, price, image });
+        if (!req.session.userId) {
+            return res.redirect('/signin');
+        }
+        const image = req.file.buffer.toString('base64');
+        const { title, price } = req.body;
+        await productsRepo.create({ title, price, image });
 
-    res.redirect('/admin/products');
-});
+        res.redirect('/admin/products');
+    });
 
 router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
     const product = await productsRepo.getOne(req.params.id);
     if (!product) {
         return res.send('Product not found');
     }
-    res.send(productsEditTemplate({product}));
+    res.send(productsEditTemplate({ product }));
 });
 
-router.post('/admin/products/:id/edit', 
-    requireAuth, 
-    upload.single('image'), 
-    [requireTitle, requirePrice], 
+router.post('/admin/products/:id/edit',
+    requireAuth,
+    upload.single('image'),
+    [requireTitle, requirePrice],
     handleErrors(productsEditTemplate, async (req) => {
         const product = await productsRepo.getOne(req.params.id);
         return { product };
-    }), 
+    }),
     async (req, res) => {
 
-    const changes = req.body;
-    if (req.file) {
-        changes.image = req.file.buffer.toString('base64');
-    }
-    try {
-        await productsRepo.update(req.params.id, changes);
-    } catch (err) {
-        return res.send('Could not find item');
-    }
+        const changes = req.body;
+        if (req.file) {
+            changes.image = req.file.buffer.toString('base64');
+        }
+        try {
+            await productsRepo.update(req.params.id, changes);
+        } catch (err) {
+            return res.send('Could not find item');
+        }
+        res.redirect('/admin/products');
+    });
+
+router.post('/admin/products/:id/delete', requireAuth, async (req, res) => {
+    await productsRepo.delete(req.params.id);
     res.redirect('/admin/products');
 });
 
