5651075105a3f3a396ab370350119288060c8d98
oxford026 <153386275+oxford026@users.noreply.github.com>
2025-03-24 23:28:03 +0300

    Update

--- routes/carts.js
+++ routes/carts.js
@@ -32,7 +32,7 @@ router.post('/cart/products', async (req, res) => {
     items: cart.items
   });
 
-  res.send('Product added to cart');
+  
 });
 
 // Receive a GET request to show all items in cart
